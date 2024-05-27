ad02067733ef82c8373f80b8841bce825e6e6478
Lily Jeong <superfreak.co.kr@gmail.com>
2024-05-27 20:59:46 +0900

    todo detail single view

--- todo/serializers.py
+++ todo/serializers.py
@@ -6,3 +6,8 @@ class TodoSimpleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'complete', 'important')
+
+class TodoDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'created', 'complete', 'important')
