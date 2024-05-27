6ca810046ee1dde9e79429caa79443eda129cd21
Lily Jeong <superfreak.co.kr@gmail.com>
2024-05-27 18:19:47 +0900

    serializer & 전체 조회 뷰

---
+++ todo/serializers.py
@@ -0,0 +1,8 @@
+# 전체 조회용 시리얼라이저. Todo 설명을 제외한 제목, 완료여부, 중요여부가 필요.
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'complete', 'important')
